8d7ad01bcc3cd32e6cdfb19265d29bd8e078116a
Greg Demko <gregory.demko@my.uwrf.edu>
2024-07-12 14:52:45 -0500

    Complete Javascript exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, i) {
+  repeatedString = "";
+  if (i < 0){
+    return "ERROR";
+  }
+  for(i; i>0; i--){
+     repeatedString += string;
+  }
+  return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
