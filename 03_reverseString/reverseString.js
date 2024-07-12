a141c5e7da400ff11bfbd1140980289ee3640899
Greg Demko <gregory.demko@my.uwrf.edu>
2024-07-12 15:02:02 -0500

    COmplete javascript exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+   const splitString = string.split("");
+   if (string == ""){
+    return string;
+   }
+   let reversed = [];
+   for(let i = splitString.length; i>0; i--){
+      reversed += splitString.pop();
+   }
+   return reversed;
 };
 
 // Do not edit below this line
