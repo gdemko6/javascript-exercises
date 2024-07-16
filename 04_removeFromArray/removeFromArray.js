c30950b1a508a186b96d22612fbd37962b5231e3
Greg Demko <gregory.demko@my.uwrf.edu>
2024-07-15 20:12:42 -0500

    Complete excercise 5 of javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, valueToRemove) {
+    let index = array.indexOf(valueToRemove);
+    return array.slice(0, index).concat(array.slice(index+1));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    let lower = 0;
+    let upper = 0;
 
+    //make sure user entered a number
+    if(typeof(numOne) != typeof(lower) || typeof(numTwo) != typeof(lower)){
+        return "ERROR";
+    }
+
+    if(numOne < 0 || numTwo < 0){
+        return "ERROR";
+    }
+
+    //find the lower number
+    if(numOne <= numTwo){
+        lower = numOne;
+        upper = numTwo
+    } else {lower=numTwo; upper=numOne;}
+
+
+    while (lower <= upper){
+        sum += lower;
+        lower++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
