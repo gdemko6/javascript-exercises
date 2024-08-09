c623e7c397ce121dd85cea903aeb8b8b5f90f462
Greg Demko <gregory.demko@my.uwrf.edu>
2024-08-09 11:43:14 -0500

    complete js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const sortByAge = people.sort((a, b) => {
+        if (typeof a.yearOfDeath === 'undefined') a.yearOfDeath = new Date().getFullYear();
+        if (typeof b.yearOfDeath === 'undefined') b.yearOfDeath = new Date().getFullYear();
+        return (a.yearOfDeath-a.yearOfBirth) > (b.yearOfDeath-b.yearOfBirth) ? -1: 1;
+    })
+
+    return sortByAge[0];
 };
 
 // Do not edit below this line
