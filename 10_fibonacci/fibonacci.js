0861469ad8bf7c2ef9ac4e6b0b7fb28b16120c12
Greg Demko <gregory.demko@my.uwrf.edu>
2024-08-09 11:11:27 -0500

    Finish exercise 10 of js exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(sequences) {
 
+    sequences=Number(sequences);
+
+    if(sequences===0) return 0;
+    
+    if(sequences<0) return "OOPS";
+
+    let fibo = 1;
+    let fibo2 = 1;
+    for(let i=2; i<sequences; i++){
+      let helper = fibo + fibo2;
+      fibo2=fibo;
+      fibo=helper;
+    }
+    return fibo;
+    
 };
 
 // Do not edit below this line
